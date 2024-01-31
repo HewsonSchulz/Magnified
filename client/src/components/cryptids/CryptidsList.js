580577adda284fadfc4fd2a1e2d440d8c42caaea
Hewson Schulz <hewsonschulz@gmail.com>
2024-01-31 09:49:19 -0600

    cryptid alphabetic sorting functionality

--- client/src/components/cryptids/CryptidsList.js
+++ client/src/components/cryptids/CryptidsList.js
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react'
 import { getCryptids } from '../../managers/cryptidManager'
 import { Cryptid } from './Cryptid'
+import { sortAlphabetically } from '../../helper'
 
 export const CryptidsList = () => {
   const [cryptids, setCryptids] = useState([])
 
   useEffect(() => {
-    getCryptids().then(setCryptids)
+    getCryptids().then((cryptids) => {
+      setCryptids(sortAlphabetically(cryptids, 'name'))
+    })
   }, [])
 
   return (
